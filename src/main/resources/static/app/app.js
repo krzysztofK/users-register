90066d9022e086b5796514115a2f084486b454c0
Krzysztof Wilaszek <Krzyszto f.Wilaszek@sabre.com>
2016-02-20 00:36:31 +0100

    Successful registration processing in UI

--- src/main/resources/static/app/app.js
+++ src/main/resources/static/app/app.js
@@ -1,4 +1,19 @@
 (function (angular) {
     angular.module("usersRegister.controllers", []);
-    angular.module("usersRegister", ["usersRegister.controllers"]);
+    var usersRegister = angular.module("usersRegister", ["ngRoute", "usersRegister.controllers"]);
+    usersRegister.config(['$routeProvider',
+        function($routeProvider) {
+            $routeProvider.
+            when('/register', {
+                templateUrl: 'partials/registerUser.html',
+                controller: 'UsersRegisterController'
+            }).
+            when('/userRegistered', {
+                templateUrl: 'partials/userRegistered.html',
+                controller: 'UserRegisteredController'
+            }).
+            otherwise({
+                redirectTo: '/register'
+            });
+        }]);
 }(angular));
--- src/main/resources/static/app/controllers.js
+++ src/main/resources/static/app/controllers.js
@@ -1,10 +1,22 @@
 (function (angular) {
-    var UsersRegisterController = function ($scope) {
+    var UsersRegisterController = function ($scope, $http, $location, $rootScope) {
         $scope.user = {name: '', password: ''};
         $scope.register = function () {
             console.log($scope.user);
+            $http.post('/register', $scope.user).then(function (response) {
+                console.log("sukces");
+                $rootScope.user = $scope.user;
+                $location.path("/userRegistered")
+            }, function (response) {
+                console.log("kicha");
+            });
         };
     };
-    UsersRegisterController.$inject = ['$scope'];
-    angular.module("usersRegister.controllers").controller("UsersRegisterController", UsersRegisterController);
+    var UserRegisteredController = function ($scope) {
+    };
+    UsersRegisterController.$inject = ['$scope', '$http', '$location', '$rootScope'];
+    UserRegisteredController.$inject = ['$scope'];
+    var module = angular.module("usersRegister.controllers");
+    module.controller("UsersRegisterController", UsersRegisterController);
+    module.controller("UserRegisteredController", UserRegisteredController);
 }(angular));
